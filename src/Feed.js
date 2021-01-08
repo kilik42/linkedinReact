f9e7a294833f4fe1534cf2ee60f026671096c381
kilik42 <marvinevins@yahoo.com>
2021-01-07 20:50:47 -0600

    added extra icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Feed.css';
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import InputOption from './InputOption';
+import  SubscriptionsIcon  from '@material-ui/icons/Subscriptions';
+import  EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon  from '@material-ui/icons/CalendarViewDay';
 
 function Feed() {
     return (
@@ -12,6 +17,28 @@ function Feed() {
                         <input type="text"/>
                             <button type= 'submit'>Send</button>
                     </form>
+                </div>
+
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} 
+                    title='Photo' 
+                    color="#70B5F9"/>
+
+                     <InputOption Icon={SubscriptionsIcon} 
+                    title='VVideo' 
+                    color="#e7A33E"/>
+
+                     <InputOption Icon={EventNoteIcon} 
+                    title='Event' 
+                    color="#C0CBCD"/> 
+                    
+                    <InputOption Icon={CalendarViewDayIcon} 
+                    title='Write Article' 
+                    color="#7FC15E"/>
+
+
+                    
+
                 </div>
             </div>
         </div>
